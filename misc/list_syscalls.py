242e997a0ae3c99ccfab9ace4a1a5f44d61efecf
Andrey Rodchenko <andrey.rodchenko@manchester.ac.uk>
2017-03-03 16:34:23 +0000

    [build] Fixed syscalls identification on 64-bit Ubuntu. If /usr/include/asm/unistd.h does not exist, processing /usr/include/x86_64-linux-gnu/asm/unistd.h instead.

--- misc/list_syscalls.py
+++ misc/list_syscalls.py
@@ -2,6 +2,10 @@
 # Produces a list of syscalls in the current system
 import os, re
 syscallCmd = "gcc -E -dD /usr/include/asm/unistd.h | grep __NR"
+if (os.path.exists("/usr/include/asm")):
+    syscallCmd = "gcc -E -dD /usr/include/asm/unistd.h | grep __NR"
+else:
+    syscallCmd = "gcc -E -dD /usr/include/x86_64-linux-gnu/asm/unistd.h | grep __NR"
 syscallDefs = os.popen(syscallCmd).read()
 sysList = [(int(numStr), name) for (name, numStr) in re.findall("#define __NR_(.*?) (\d+)", syscallDefs)]
 denseList = ["INVALID"]*(max([num for (num, name) in sysList]) + 1)
